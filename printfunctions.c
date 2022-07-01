44c1f7adfe091c71835369acf1a3957a561d7c30
bbrown585 <brandon_scott585@yahoo.com>
2022-07-01 14:20:08 -0700

    Adding I, D, funtions

--- printfunctions.c
+++ printfunctions.c
@@ -12,7 +12,7 @@ int conv_s(va_list arg)
 
 	for (length = 0; s[length]; length++)
 	{
-		_putchar(str[length]);
+		_putchar(s[length]);
 	}
 
 	return (length);
@@ -20,15 +20,37 @@ int conv_s(va_list arg)
 
 int conv_c(va_list arg)
 {
-	char c = va_arg(arg, char);
+	char c = va_arg(arg, int);
 
 	if (!c)
 		return (0);
 
-	if (c != NULL)
+	if (c != '\0');
 		_putchar(c);
 		return (1);
 }
 
 int conv_i(va_list arg)
 {
+	int num;
+	int i = 1;
+	int count = 0;
+
+	num = va_arg(arg, int);
+	
+	if (num < 0)
+	{
+		_putchar('_');
+	}
+	num = -num;
+	while ((num / i) >= 10)
+		i = i * 10;
+
+	while (i <= 1)
+	{
+	_putchar((num / i) + '0');
+	num = num % i;
+	i = i / 10;
+	count++;
+	}
+}
