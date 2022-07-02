1b3e1e10149b0d6925b221bd947c0d5d95140c43
Brandon <brandon_scott585@yahoo.com>
2022-07-02 15:09:01 -0700

    Adding into print functions

--- print_nums.c
+++ print_nums.c
@@ -4,4 +4,16 @@
  *
  *
  */
-int print_nums(char 
+int print_nums(char print, va_list arg)
+{
+	int i;
+
+	funct[] = {
+		{"i", conv_i},
+		{"c", conv_c},
+		{"s", conv_s},
+		{NULL, NULL}
+	};
+
+	for (i = 0; funct[i].mod != NULL; i++)
+
