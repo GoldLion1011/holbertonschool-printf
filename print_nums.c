c9fa24b1fb1d465508b3f8c3c526fe7ba7dd8339
Brandon <brandon_scott585@yahoo.com>
2022-07-03 15:17:02 -0700

    adding into the print functions small change at bottom

--- print_nums.c
+++ print_nums.c
@@ -1,19 +1,28 @@
 #include "main.h"
 
 /**
- *
- *
+ * print_nums - prints specific function
+ * @arg: format
+ * @print: print
+ * Return: a function
  */
-int print_nums(char print, va_list arg)
+int print_nums(char *print, va_list arg)
 {
 	int i;
 
-	funct[] = {
+	flag funct[] = {
 		{"i", conv_i},
 		{"c", conv_c},
 		{"s", conv_s},
+		{"d", conv_d},
 		{NULL, NULL}
 	};
 
 	for (i = 0; funct[i].mod != NULL; i++)
+	{
+		if (funct[i].mod[0] == print)
+			return (funct[i].f(arg));
+	}
+	return (0);
+}
 
