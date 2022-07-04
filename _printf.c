494913f16dd43d517507f7c3356bb601c95012a0
Brandon <brandon_scott585@yahoo.com>
2022-07-04 10:19:44 -0700

    Adding va_list to printf

--- _printf.c
+++ _printf.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
- * _printf - print strings and int
- * @format: specifier
+ * _printf - print strings and int to stdout
+ * @format: specifier to print
  *
- *
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	char i;
+	int counter = 0;
+	va_list arg;
+
+	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
 
 	while (format[i] != '\0')
 	{
